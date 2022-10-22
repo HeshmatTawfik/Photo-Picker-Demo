b3fe914b691d6d24a7b0a9ec1244245666c161e9
HeshmatTawfik <heshmattawfik@gmail.com>
2022-10-22 19:52:32 +0200

    Pick photo from gallery and show it in image view

--- app/src/main/java/com/heshmat/photopickerdemo/MainActivity.kt
+++ app/src/main/java/com/heshmat/photopickerdemo/MainActivity.kt
@@ -2,10 +2,38 @@ package com.heshmat.photopickerdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia
+import com.heshmat.photopickerdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val pickMedia = registerForActivityResult(PickVisualMedia()) { uri ->
+            if (uri == null) {
+                Log.e(TAG, "uri is null")
+            } else {
+                Log.i(TAG, "uri = $uri")
+                binding.ivPickedPhoto.setImageURI(uri)
+                binding.ivPickedPhoto.visibility = View.VISIBLE
+
+            }
+
+        }
+        binding.btnOpenPicker.setOnClickListener {
+            pickMedia.launch(PickVisualMediaRequest(PickVisualMedia.ImageOnly))
+        }
+
     }
 }
